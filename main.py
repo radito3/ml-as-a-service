21ec8ef78d459ee2c9c95a6670712580e0ea5ac3
Rangel Ivanov <rangel.ivanov@sap.com>
2023-06-05 22:32:02 +0300

    Iron out bugs and add /evaluate endpoint

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import model as nn
 import datasets as ds
 import numpy as np
-from fastapi import FastAPI, HTTPException, status, UploadFile, Response
+from fastapi import FastAPI, HTTPException, status, Response
 from pydantic import BaseModel
 import json
 import asyncio
 import uuid
-import aiofiles
 
 app = FastAPI()
 
@@ -21,10 +20,14 @@ class DatasetName(BaseModel):
     name: str
 
 
+class NumClassesBody(BaseModel):
+    num_classes: int
+
+
 async def train_model(model: nn.NeuralNetwork, ds_name: DatasetName):
-    dataset = datasets[ds_name]()
-    x = dataset.x
-    y = dataset.y
+    dataset = datasets[ds_name.name]()
+    x = dataset['x']
+    y = dataset['y']
     num_classes = len(np.unique(y))
     x_train, x_test, y_train, y_test = model.split_data(x, y)
     x_train, _, y_train, _ = model.prepare_data(x_train, x_test, y_train, y_test, num_classes)
@@ -42,26 +45,9 @@ async def get_datasets():
     return [*datasets]
 
 
-@app.post("/datasets/upload")
-async def upload_dataset(file: UploadFile):
-    if not file.filename.endswith(".csv"):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File must be a csv")
-
-    try:
-        async with aiofiles.open(f'/app/data/datasets/{file.filename}', mode='wb', buffering=4096) as f:
-            while buff := await file.read(4096):
-                await f.write(str(buff))
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'failed to persist dataset: {e}')
-    finally:
-        await file.close()
-
-    return {"status": "uploaded"}
-
-
 @app.post("/models", status_code=status.HTTP_201_CREATED)
-async def create_model(num_classes: int):
-    m = nn.NeuralNetwork(num_classes)
+async def create_model(num_classes: NumClassesBody):
+    m = nn.NeuralNetwork(num_classes.num_classes)
     model_id = str(uuid.uuid4())
     models_cache[model_id] = {"layers": m.num_layers,
                               "neurons": m.num_neurons_per_layer,
@@ -76,7 +62,7 @@ async def get_model(model_id: str):
         raise HTTPException(status_code=404, detail=f'Model {model_id} not found')
 
     m = models_cache[model_id]
-    return {"layers": m.layers, "neurons": m.neurons, "state": m.state}
+    return {"layers": m["layers"], "neurons": m["neurons"], "state": m["state"]}
 
 
 @app.post("/models/{model_id}/fit", status_code=status.HTTP_202_ACCEPTED)
@@ -84,14 +70,14 @@ async def fit_model(model_id: str, ds_name: DatasetName):
     if model_id not in models_cache:
         raise HTTPException(status_code=404, detail=f'Model {model_id} not found')
 
-    if ds_name not in datasets:
-        raise HTTPException(status_code=400, detail=f'Invalid dataset: {ds_name}')
+    if ds_name.name not in datasets:
+        raise HTTPException(status_code=400, detail=f'Invalid dataset: {ds_name.name}')
 
     if model_id in jobs:
-        raise HTTPException(status_code=400, detail=f'Model {ds_name} training already started')
+        raise HTTPException(status_code=400, detail=f'Model {ds_name.name} training already started')
 
     m = models_cache[model_id]
-    task = asyncio.create_task(train_model(m, ds_name))
+    task = asyncio.create_task(train_model(m["obj"], ds_name))
     jobs[model_id] = task
     return {"status": "started training"}
 
@@ -101,7 +87,7 @@ async def model_predict(model_id: str, ds_name: DatasetName, response: Response)
     if model_id not in models_cache:
         raise HTTPException(status_code=404, detail=f'Model {model_id} not found')
 
-    if ds_name not in datasets:
+    if ds_name.name not in datasets:
         raise HTTPException(status_code=400, detail=f'Invalid dataset: {ds_name}')
 
     if model_id not in jobs:
@@ -118,13 +104,47 @@ async def model_predict(model_id: str, ds_name: DatasetName, response: Response)
         raise HTTPException(status_code=500, detail=f'Model {model_id} training error: {e}')
 
     m = models_cache[model_id]
-    model = m.obj
-    ds = datasets[ds_name]()
-    x = ds.x
-    y = ds.y
+    model = m["obj"]
+    ds = datasets[ds_name.name]()
+    x = ds['x']
+    y = ds['y']
     num_classes = len(np.unique(y))
     x_train, x_test, y_train, y_test = model.split_data(x, y)
     _, x_test, _, _ = model.prepare_data(x_train, x_test, y_train, y_test, num_classes)
 
     result = model.predict(x_test)
     return json.dumps(result.tolist())
+
+
+@app.post("/models/{model_id}/evaluate")
+async def model_evaluate(model_id: str, ds_name: DatasetName, response: Response):
+    if model_id not in models_cache:
+        raise HTTPException(status_code=404, detail=f'Model {model_id} not found')
+
+    if ds_name.name not in datasets:
+        raise HTTPException(status_code=400, detail=f'Invalid dataset: {ds_name}')
+
+    if model_id not in jobs:
+        raise HTTPException(status_code=400, detail=f'Model {ds_name} not trained')
+
+    job = jobs[model_id]
+    if not job.done():
+        response.status_code = 204
+        return {"status": "training"}
+
+    try:
+        _ = job.result()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Model {model_id} training error: {e}')
+
+    m = models_cache[model_id]
+    model = m["obj"]
+    ds = datasets[ds_name.name]()
+    x = ds['x']
+    y = ds['y']
+    num_classes = len(np.unique(y))
+    x_train, x_test, y_train, y_test = model.split_data(x, y)
+    _, x_test, _, y_test = model.prepare_data(x_train, x_test, y_train, y_test, num_classes)
+
+    accuracy, loss = model.evaluate(x_test, y_test)
+    return {"accuracy": accuracy, "loss": loss}
